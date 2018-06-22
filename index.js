e2f3d20b2c3cd9bebd2c1d94e4189b0bcd1d64c1
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-22 13:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key] : value})
+}
+function updateObjectWithKeyAndValue(object, key,value){
+
+}
