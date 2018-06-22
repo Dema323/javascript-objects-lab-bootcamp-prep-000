4e8094d3e80cd7b91e8e36a873607f59c234e99e
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-22 13:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ return object
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key] : value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+
+ return object
+}
